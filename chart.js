f435dc2cc9f5d223a286fb003107ba4b31585e2c
Aviola254 <aviolaavidar307@gmail.com>
2026-03-22 18:06:09 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -4,9 +4,38 @@ function startTicks(symbol){
 
 if(!ws){
 
-connectAPI("");
+ws = new WebSocket(
+"wss://ws.derivws.com/websockets/v3?app_id=1089"
+);
+
+ws.onopen = function(){
+
+ws.send(JSON.stringify({
+ticks: symbol,
+subscribe: 1
+}));
+
+};
+
+ws.onmessage = function(msg){
+
+let data = JSON.parse(msg.data);
+
+if(data.msg_type === "tick"){
+
+ticks.push(data.tick.quote);
+
+if(ticks.length > 20){
+ticks.shift();
+}
+
+drawChart();
 
 }
+
+};
+
+}else{
 
 ws.send(JSON.stringify({
 ticks: symbol,
@@ -14,16 +43,6 @@ subscribe: 1
 }));
 
 }
-
-function handleTick(data){
-
-ticks.push(data.tick.quote);
-
-if(ticks.length > 30){
-ticks.shift();
-}
-
-drawChart();
 
 }
 
